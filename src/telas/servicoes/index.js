9e0f70426aca81e7633f54cb491803b4e96452ac
Lia Dias <liadias914@gmail.com>
2023-03-10 17:48:25 -0300

    Cria uma estrutura de telas
    Cria a tela de serviços e remove tudo na tela inicial chamando apenas esta tela

---
+++ src/telas/servicoes/index.js
@@ -0,0 +1,10 @@
+import { Text, SafeAreaView, StatusBar } from 'react-native';
+
+export default function Servicos() {
+    return (
+        <SafeAreaView>
+            <StatusBar/>
+            <Text> Gatito!!! </Text>
+        </SafeAreaView>
+    )
+}
