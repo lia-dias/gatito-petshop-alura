610e4819e318a377ad6a325e699656516d3a2327
Lia Dias <liadias914@gmail.com>
2023-03-10 18:03:07 -0300

    Add Item serviço e renderização
    - Define o componente Item de serviço
    - Adiciona alguns mock de itens locais em Serviço
    - Renderiza os mock de itens de acordo com o componente Item

---
+++ src/telas/servicos/index.js
@@ -0,0 +1,41 @@
+import { Text, SafeAreaView, StatusBar, FlatList } from 'react-native';
+import Item from './Item';
+
+const servicos = [
+    {
+        id: 1,
+        nome: 'Banho',
+        preco: 79.90,
+        descricao: "Banho no seu petzinho."
+    },
+    {
+        id: 2,
+        nome: 'Vacina V4',
+        preco: 89.90,
+        descricao: "Uma dose da vacina da V4. Seu gato precisa de duas."
+    },
+    {
+        id: 3,
+        nome: 'Vacina Antirrábica',
+        preco: 99.90,
+        descricao: "Uma dose da vacina antirrábica. Seu gato precisa de uma a cada 12 meses."
+    },
+];
+
+function renderServico ({ item }) {
+    return <Item {...item}></Item>;
+}
+
+export default function Servicos() {
+    return (
+        <SafeAreaView>
+            <StatusBar/>
+            <Text> Gatito!!! </Text>
+            <FlatList
+                data={servicos}
+                renderItem={renderServico}
+                keyExtractor={(servico) => servico.id}
+                />
+        </SafeAreaView>
+    )
+}
