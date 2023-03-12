c9af6dd4a5502b26c558ebde911e66ed941c9b10
Lia Dias <liadias914@gmail.com>
2023-03-12 12:05:02 -0300

    Add estilos globais e item

--- src/telas/servicos/Item/index.js
+++ src/telas/servicos/Item/index.js
@@ -1,11 +1,12 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import estilo from "./estilo";
 
 export default function Item({nome, preco, descricao}) {
     return (
-        <>
-            <Text>{nome}</Text>
-            <Text>{preco}</Text>
-            <Text>{descricao}</Text>
-        </>
+        <View style={estilo.informacao}>
+            <Text style={estilo.nome}>{nome}</Text>
+            <Text style={estilo.descricao}>{descricao}</Text>
+            <Text style={estilo.preco}>${preco}</Text>
+        </View>
     )
 }
