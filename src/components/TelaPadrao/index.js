7601d566cef1a5e571487cf0fd8c4122c718d37a
Lia Dias <liadias914@gmail.com>
2023-03-16 15:42:44 -0300

    Add estilos nas safe areas

--- src/components/TelaPadrao/index.js
+++ src/components/TelaPadrao/index.js
@@ -1,16 +1,20 @@
 import { KeyboardAvoidingView, Platform, SafeAreaView, StatusBar } from "react-native";
-import estilos from "../../estilos";
+import estiloGlobal from "../../estilos";
+import estilo from "./estilos";
 
 export default function TelaPadrao({children}) {
     return (
-        <SafeAreaView style={estilos.preencher}>
+        <>
+        <SafeAreaView style={estilo.ajusteTela}>
             <StatusBar/>
             <KeyboardAvoidingView
                 behavior={Platform.OS === 'ios' ? "padding" : "height"}
-                style={estilos.preencher}
-            >
+                style={estiloGlobal.preencher}
+                >
                 {children}
             </KeyboardAvoidingView>
         </SafeAreaView>
+        <SafeAreaView style={estilo.ajusteTelaBaixo}/>
+        </>
     )
 }
